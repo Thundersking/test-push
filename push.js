c75ab5c2d0d643a7c074a4fc1ca185541d9c5e40
sherkozhuhov.da <d.a.sherkozhukhov1995@gmail.com>
2023-09-02 13:45:30 +0300

    fix

--- push.js
+++ push.js
@@ -4,18 +4,18 @@ var push = require('web-push');
 // console.log(vapidKeys);
 
 let vapidKeys = {
-  publicKey: 'BPAal_-Esqc-KY-aFN3ka7NrizI59xsEohWRnajcbkDtR3NDltqSE1ZTwCLeaBe_eGXi0UX8vxrgbXEjtUZBH48',
-  privateKey: 'pyTd4smk6TI5oVSvtH3BMGr2hcfj_9nT-PA9GSZL8k0'
+    publicKey: 'BPAal_-Esqc-KY-aFN3ka7NrizI59xsEohWRnajcbkDtR3NDltqSE1ZTwCLeaBe_eGXi0UX8vxrgbXEjtUZBH48',
+    privateKey: 'pyTd4smk6TI5oVSvtH3BMGr2hcfj_9nT-PA9GSZL8k0'
 }
 
 push.setVapidDetails('mailto:d@vionvi.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
 let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/cxKN2AtjPKI:APA91bH7w2jCvVdjEG0ZHrp_2z8ONELnSFrmWq68s_JknbWGwwtRpFxsd1A6kFni6evVFrN0vR0FUAkRB53_ZWBBYxPlwFumJToj5ZE2q1SQfdAHXKmYQXB7ruhT7PLYn1-yaOAtXWFF",
+    "endpoint": "https://fcm.googleapis.com/fcm/send/fTWxhj0i5pc:APA91bEtBhcj4kPGPllO4Gs0VQUQh2RIVVnytKiS7Dk5foFbhavWwN0x8GbGnUoKVNc3w3yZKVFPcdpEVW2bPtJ4zlTwEgwCwPQiUoC3vSv9Ocjihb4hpNWvtujWHd_sriWobAzo449a",
     "expirationTime": null,
     "keys": {
-        "p256dh": "BCTJci2Nh5M-CnylY6tDroWOQ-BlMajLOckFTathBfy4eYuaSB8mmcTqyG2FnlLV_xdmsiP_aRhdVXq8t2WYjKA",
-        "auth": "Ad2-MdGco8WizmjLqcaHDQ"
+        "p256dh": "BCaMeE7um7UBHIcVugWcg03LEI6bgOPusHj-2wIMeoeX_EHLYh--uIVwfB597-LxExtqF0A4O00OpBZ5o4PCGF0",
+        "auth": "G0gwOT2LW2rMOf7tJOB7yQ"
     }
 }
 
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,29 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('test message', {})
-})
+// self.addEventListener('push', () => {
+//     self.registration.sendNotification('test message', {})
+// })
+
+self.addEventListener('push', function (e) {
+    var options = {
+        body: 'Thin notification was generated from a push!',
+        icon: '/img/icons/android-chrome-192x192.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close', title: 'Close',
+                icon: 'images/xmark.png'
+            }
+        ]
+    };
+
+    e.waitUntil(
+        self.registration.showNotification('Тестовое уведомление 3', options)
+    );
+});
